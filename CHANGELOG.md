19b9dce56619fc05819bdd191f71111f5ab5a86d
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2017-10-01 20:49:58 +0200

    ADD: handle server role names and server number

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## 0.2 2017-10-01
+
+* ADD: handle server role name and server number
+* ADD: print help for `--help` or `-h` argument
+
+## 0.1 2017-09-29
+
+* Initial version of a script
--- bin/cap-ssh-to-env.sh
+++ bin/cap-ssh-to-env.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-# Capistrano SSH v0.1 2017-09-29
+
+# Capistrano SSH v0.2 2017-10-01
 
 CAT=`which cat`
 CUT=`which cut`
 GREP=`which grep`
 HEAD=`which head`
 SSH=`which ssh`
+TAIL=`which tail`
+
+function show_help {
+  echo ""
+  echo "Usage: "
+  echo "$0 [role] [server_number]"
+  echo ""
+}
+
+if [ "x$1" == "x--help" ] || [ "x$1" == "x-h" ]; then
+  show_help
+  exit 1
+fi
 
 CAPISTRANO_CONFIG_DIR="config/deploy"
 
@@ -14,11 +28,23 @@ filename=$0
 REMOTE_ENV="${filename##*-}"
 REMOTE_ENV="${REMOTE_ENV%.*}"
 
-echo "Remote ENV: ${REMOTE_ENV}"
+SERVER_ROLE="$1"
+SERVER_NO="$2"
+if [ "x${SERVER_NO}" == "x" ]; then
+  SERVER_NO="1"
+fi
 
-FIRST_SERVER_LINE=`${CAT} ${CAPISTRANO_CONFIG_DIR}/${REMOTE_ENV}.rb  | ${GREP} "server" | ${HEAD} -1`
+echo "Remote ENV: ${REMOTE_ENV}"
+echo "Server role: ${SERVER_ROLE}"
+echo "Server number: ${SERVER_NO}"
+
+FIRST_SERVER_LINE=`${CAT} ${CAPISTRANO_CONFIG_DIR}/${REMOTE_ENV}.rb  | ${GREP} "server" | ${GREP} "${SERVER_ROLE}" | ${HEAD} -${SERVER_NO} | ${TAIL} -1`
+
 REMOTE_HOST=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 1 -d "," | ${CUT} -f 2 -d "'"`
 REMOTE_USER=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 2 -d "," | ${CUT} -f 2 -d "'"`
+
+echo "Remote host: ${REMOTE_HOST}"
+echo "Remote user: ${REMOTE_USER}"
 
 ${SSH} ${REMOTE_USER}@${REMOTE_HOST} -v
 
