67404d477aba0999fe9727e0528ae7aea30e0034
Grzegorz Błaszczyk <grzegorz.blaszczyk@goodylabs.com>
2023-12-06 15:00:59 +0100

    FIX: parse single quotes and double quotes

--- bin/cap-ssh-to-env.sh
+++ bin/cap-ssh-to-env.sh
@@ -40,8 +40,8 @@ echo "Server number: ${SERVER_NO}"
 
 FIRST_SERVER_LINE=`${CAT} ${CAPISTRANO_CONFIG_DIR}/${REMOTE_ENV}.rb  | ${GREP} -v "^#" | ${GREP} "server" | ${GREP} "${SERVER_ROLE}" | ${HEAD} -${SERVER_NO} | ${TAIL} -1`
 
-REMOTE_HOST=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 1 -d "," | ${CUT} -f 2 -d "'"`
-REMOTE_USER=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 2 -d "," | ${CUT} -f 2 -d "'"`
+REMOTE_HOST=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 1 -d "," | ${CUT} -f 2 -d "'" | ${CUT} -f 2 -d "\""`
+REMOTE_USER=`echo ${FIRST_SERVER_LINE} | ${CUT} -f 2 -d "," | ${CUT} -f 2 -d "'" | ${CUT} -f 2 -d "\""`
 
 echo "Remote host: ${REMOTE_HOST}"
 echo "Remote user: ${REMOTE_USER}"
